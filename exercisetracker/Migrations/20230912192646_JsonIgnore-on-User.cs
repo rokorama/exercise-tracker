14db0f111b069a535ed207981bed8a97ea296bdc
Rokas <rokamanauskas@gmail.com>
2023-09-12 23:07:26 +0300

    add unnecessary migrations to keep history up-to-date

---
+++ exercisetracker/Migrations/20230912192646_JsonIgnore-on-User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exercisetracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class JsonIgnoreonUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
