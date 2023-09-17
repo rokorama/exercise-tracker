586b5b9b053b9db08cf1663f932dec64805d2b2b
Rokas <rokamanauskas@gmail.com>
2023-09-17 19:10:47 +0300

    add extra props to the User class

---
+++ exercisetracker/Migrations/20230917160429_Expand-User-class.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exercisetracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpandUserclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Weight",
+                table: "Users",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Users");
+        }
+    }
+}
--- exercisetracker/Models/User.cs
+++ exercisetracker/Models/User.cs
@@ -4,5 +4,9 @@ public class User
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public int? Height { get; set; }
+    public int? Weight { get; set; }
     public virtual List<WorkoutSession> WorkoutSessions { get; set; } = new List<WorkoutSession>();
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+    public string Role { get; set; } = "user";
 }
