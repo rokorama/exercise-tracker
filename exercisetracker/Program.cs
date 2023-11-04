f0baba868a0d3229ff740161450e7eec25de3d92
Rokas <rokamanauskas@gmail.com>
2023-11-04 16:45:44 +0200

    move db connection string to .env

--- exercisetracker/Program.cs
+++ exercisetracker/Program.cs
@@ -1,14 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using exercisetracker.Data;
-using exercisetracker.Services;
+using exercisetracker.Services.WorkoutService;
 
 var builder = WebApplication.CreateBuilder(args);
 
+DotNetEnv.Env.Load();
 // Add services to the container.
-
-builder.Services.AddDbContext<DataContext>(options => 
+builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DB_CONN_STRING"));
 });
 
 builder.Services.AddControllersWithViews();
@@ -25,20 +25,20 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+// app.MapGet("api/test", () => new { Test = "hello i am api" });
 
-app.MapGet("api/test", () => new { Test = "hello i am api" });
-
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "api/{controller}/{id?}");
-
-
-app.UseSwagger();
-app.UseSwaggerUI();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "api/{controller}/{id?}");
 
 app.MapFallbackToFile("index.html");
 
